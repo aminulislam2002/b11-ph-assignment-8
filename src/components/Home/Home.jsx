c60ceec3a6602546681d378fe05227467c260b0b
aminulislam2002 <aminulislamemon2002@gmail.com>
2025-04-23 02:10:51 +0600

    we provides best law services

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import LawServices from "../LawServices/LawServices";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-white text-black">
+      <div className="w-full max-w-[1280px] mx-auto">
+        <LawServices></LawServices>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/LawServices/LawServices.jsx
@@ -0,0 +1,43 @@
+import image1 from "../../assets/success-doctor.png";
+import image2 from "../../assets/success-review.png";
+import image3 from "../../assets/success-patients.png";
+import image4 from "../../assets/success-staffs.png";
+
+const LawServices = () => {
+  return (
+    <div className="relative w-full h-full py-[100px]">
+      <h1 className="text-center text-[40px] font-extrabold mb-4">We Provide Best Law Services</h1>
+      <p className="text-center text-base font-normal text-[#0F0F0F] mb-8">
+        Our platform connects you with verified, experienced Lawyers across various specialities — all at your convenience.{" "}
+      </p>
+
+      <div className="grid grid-cols-4 gap-6">
+        <div className="w-full h-full p-10 bg-[#0F0F0F] bg-opacity-5 rounded-md border border-[#0F0F0F] border-opacity-15 flex flex-col justify-center items-center gap-2.5">
+          <img src={image1} alt="" />
+          <h3 className="text-[48px] font-extrabold">199+</h3>
+          <p className="text-base font-normal text-[#0F0F0F]">Total Lawyer</p>
+        </div>
+
+        <div className="w-full h-full p-10 bg-[#0F0F0F] bg-opacity-5 rounded-md border border-[#0F0F0F] border-opacity-15 flex flex-col justify-center items-center gap-2.5">
+          <img src={image2} alt="" />
+          <h3 className="text-[48px] font-extrabold">467+</h3>
+          <p className="text-base font-normal text-[#0F0F0F]">Total Reviews</p>
+        </div>
+
+        <div className="w-full h-full p-10 bg-[#0F0F0F] bg-opacity-5 rounded-md border border-[#0F0F0F] border-opacity-15 flex flex-col justify-center items-center gap-2.5">
+          <img src={image3} alt="" />
+          <h3 className="text-[48px] font-extrabold">1900+</h3>
+          <p className="text-base font-normal text-[#0F0F0F]">Cases Initiated</p>
+        </div>
+
+        <div className="w-full h-full p-10 bg-[#0F0F0F] bg-opacity-5 rounded-md border border-[#0F0F0F] border-opacity-15 flex flex-col justify-center items-center gap-2.5">
+          <img src={image4} alt="" />
+          <h3 className="text-[48px] font-extrabold">300+</h3>
+          <p className="text-base font-normal text-[#0F0F0F]">Total Stuffs</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LawServices;
