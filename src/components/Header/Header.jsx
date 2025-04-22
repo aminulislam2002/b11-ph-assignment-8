f062775a77970093595047d5fb400cf16b574cef
aminulislam2002 <aminulislamemon2002@gmail.com>
2025-04-23 01:49:08 +0600

    design header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,38 @@
-
+import { Link } from "react-router";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="w-ful bg-white">
+      <div className="navbar max-w-[1280px] mx-auto">
+        <div className="navbar-start flex justify-start items-center gap-2.5">
+          <img src={logo} alt="Low.bd" />
+          <Link to="/" className="text-2xl text-black font-bold">
+            Law.bd
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li className="text-black">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="text-black">
+              <Link>My Booking</Link>
+            </li>
+            <li className="text-black">
+              <Link>Blogs</Link>
+            </li>
+            <li className="text-black">
+              <Link>Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <button className="bg-[#0EA106] text-white text-base py-[15px] px-[30px] rounded-full">Contact Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
